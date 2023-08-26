f66115ba3f1b828b76eb92f65d7c4820830092ea
Rishab Pal <105929270+brokenShinobi@users.noreply.github.com>
2023-08-26 20:35:59 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,8 +4,8 @@ import { Roboto } from "next/font/google";
 const roboto = Roboto({ weight: ["400", "700"], subsets: ["latin"] });
 
 export const metadata = {
-  title: "Frontend Mentor | Newsletter Sign Up Form With Success Message",
-  description: "Frontend Mentor | Newsletter Sign Up Form With Success Message",
+  title: "Frontend Mentor | Newsletter Sign Up Form",
+  description: "Frontend Mentor | Newsletter Sign Up Form",
 };
 
 export default function RootLayout({ children }) {
